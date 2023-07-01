4529d2ad7252bd1f7374d39f7795ef38e818e754
LorcaLince <121203711+LorcaLince@users.noreply.github.com>
2023-07-01 19:00:48 -0300

    ticketphp2

---
+++ menu.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximun-scale=1.0, minimun-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="imagenes/codoacodo.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body>
+<header>
+        <nav class="navbar navbar-expand-md bg-body-tertiary px-5 py-2 fixed-top" data-bs-theme="dark">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler"
+                    aria-controls="navbar-toggler" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarToggler">
+                    <a class="navbar-brand" href="./index.html">
+                        <img id="logo-pagina" src="./imagenes/codoacodo.png" width="100" alt="Logo de la pagina">
+                        <span class="nombre-logo">Conf Bs As</span>
+                    </a>
+                    <ul class="navbar-nav d-flex justify-content-center align-items-e">
+                        <li class="nav-item cursor-pointer">
+                            <a class="nav-link" aria-current="page" href="#confciudad">La conferencia</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#oradores">Los oradores</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#bsasoctubre">El lugar y la fecha</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link  active" href="#convierteteenorador">Conviértete en orador</a>    
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link link-verde-ticket" href="HTML/ticket.html">Comprar tickets</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+</header> 
